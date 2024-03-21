45e23254799f94cbfb723881f97799e70340e6bf
Yaman555 <163329909+Yaman555@users.noreply.github.com>
2024-03-21 11:56:01 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import google.generativeai as gen_ai
 from PyPDF2 import PdfReader
 from collections import deque  
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
